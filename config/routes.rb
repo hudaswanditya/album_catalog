4d5ed477816edf89d83f04a9022cdf87c3519687
hudaswanditya <huda.swanditya@gmail.com>
2024-12-01 23:39:52 +0700

    configure route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :albums do
+    member do
+      patch :publish
+    end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :tracks
+  end
+  
+  root "albums#index"
 end
